092e865d57fa821cc76d6869349ff1c9623eaf7c
Shane Whiteheaad <Shane.Whitehead@dayforce.com>
2025-04-10 18:23:24 +1000

    Fix the location accuracy

--- WhichBin-SwiftUI/Support/Support.swift
+++ WhichBin-SwiftUI/Support/Support.swift
@@ -15,10 +15,10 @@ class Support: NSObject {
 
     static var preferredDataSource: URL {
         guard let value = ProcessInfo.processInfo.environment["sample"], let result = Bool(value), result else {
-//            return URL(string: "https://data.gov.au/data/dataset/0af93e4d-4ef7-4d45-855b-364039c52f98/resource/172777d4-b8dc-4579-a268-acf836da4362/download/frankston-city-council-garbage-collection-zones.json")!
+            log(debug: "Load remote")
             return URL(string: "https://connect.pozi.com/userdata/frankston-publisher/Community/Kerbside_Garbage_Collection_(Widget).json")!
         }
-//        return Bundle.main.url(forResource: "Sample", withExtension: "json")!
+        log(debug: "Load local samples")
         return Bundle.main.url(forResource: "NewSample", withExtension: "json")!
     }
     
